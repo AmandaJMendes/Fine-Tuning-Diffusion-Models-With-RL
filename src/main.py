4b8b3ef9d875323c6b43f5c09917116270331ba6
AmandaJMendes-CW <amanda.mendes@cloudwalk.io>
2025-05-21 20:31:05 +0000

    feat: first version of actual codebase

---
+++ src/main.py
@@ -0,0 +1,107 @@
+import torch
+
+def generate_batch(
+    model,   
+    scheduler,
+    batch_size: int,  
+    device="cuda:0"
+) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+    """
+    Generate a batch of images from the model.
+    Returns:
+        latents: shape (B, T, C, H, W)
+        next_latents: shape (B, T, C, H, W)
+        log_probs: shape (B, T)
+        timesteps: shape (B, T)
+    """
+
+    # Start from pure noise
+    n_channels = model.config.in_channels
+    image_size = model.config.sample_size
+    latents = torch.randn((batch_size, n_channels, image_size, image_size), device=device)
+
+    # Initialize the arrays
+    log_probs_list = [] #shape: (T, B)
+    latents_list = [] #shape: (T, B, C, H, W)
+    next_latents_list = [] #shape: (T, B, C, H, W)
+    timesteps_list = [] #shape: (T)
+
+    # Generate trajectory by iterating through the diffusion process
+    for t in scheduler.timesteps:
+        # Append the latents to the list
+        latents_list.append(latents)
+
+        # Get the model prediction
+        with torch.no_grad():
+            pred_noise = model(latents, t).sample
+
+        # Step the scheduler to get the next latents
+        scheduler_output, log_prob = scheduler.step(pred_noise, t, latents)
+        latents = scheduler_output.prev_sample
+
+        # Append the log_prob and new latents to the lists
+        log_probs_list.append(log_prob)
+        next_latents_list.append(latents)
+        timesteps_list.append(t)
+
+    # Convert the lists to tensors and reshape them
+    latents = torch.stack(latents_list).to(device).permute(1, 0, 2, 3, 4) #shape: (B, T, C, H, W)
+    next_latents = torch.stack(next_latents_list).to(device).permute(1, 0, 2, 3, 4) #shape: (B, T, C, H, W)
+    log_probs = torch.stack(log_probs_list).to(device).permute(1, 0) #shape: (B, T)
+
+    timesteps = torch.tensor(timesteps_list, device=device)
+    timesteps = torch.broadcast_to(timesteps, (batch_size, len(timesteps_list))) #shape: (B, T)
+
+    return latents, next_latents, log_probs, timesteps
+
+def rescore_batch(
+    model,
+    scheduler,
+    latents: torch.Tensor, #shape: (B, C, H, W)
+    next_latents: torch.Tensor, #shape: (B, C, H, W)
+    timesteps: torch.Tensor #shape: (B,)
+) -> torch.Tensor:
+    """
+    Compute log p(next_latents | latents) with `model` and return (B,).
+
+    Args:
+        model: the diffusion model
+        scheduler: the scheduler
+        latents: shape (B, C, H, W)
+        next_latents: shape (B, C, H, W)
+        timesteps: shape (B,)
+    """
+    # Get the model prediction
+    pred_noise = model(latents, timesteps).sample
+
+    # Step the scheduler to get the log prob of next_latents given latents  
+    _, log_prob = scheduler.step(pred_noise, timesteps, latents, next_latents)
+
+    return log_prob
+
+
+if __name__ == "__main__":
+    from diffusers import UNet2DModel
+    from custom_ddim_scheduler import CustomDDIMScheduler
+    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Load the model and scheduler
+    scheduler = CustomDDIMScheduler.from_pretrained("google/ddpm-celebahq-256", use_safetensors = True)
+    pretrained_model = UNet2DModel.from_pretrained("google/ddpm-celebahq-256").to(device)
+
+    # Generate a batch of images
+    latents, next_latents, log_probs, timesteps = generate_batch(pretrained_model, scheduler, 3, device)
+
+    # Rescore the batch and backpropagate for each timestep
+    for t in range(timesteps.shape[1]):
+        new_log_probs = rescore_batch(
+            pretrained_model, 
+            scheduler, 
+            latents[:, t], 
+            next_latents[:, t], 
+            timesteps[:, t]
+        )
+        print(new_log_probs.shape)
+        #backpropagate
+    # step
